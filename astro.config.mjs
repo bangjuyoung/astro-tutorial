f22609167d3daec551db0f7dcc736bdaf6d9e737
bangjuyoung <juyoungbang82@gmail.com>
2024-08-06 18:23:30 +0900

    feat: build astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://zippy-zuccutto-56dd0c.netlify.app/"
+  site: "https://zippy-zuccutto-56dd0c.netlify.app/",
+  integrations: [preact()]
 });
